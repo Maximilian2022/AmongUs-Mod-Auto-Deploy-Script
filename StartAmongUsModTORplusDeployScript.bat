49b02a4cb277970267dd4fe6467b7cb0b2807fc9
Maximilian2022 <99347861+Maximilian2022@users.noreply.github.com>
2022-02-10 01:09:56 +0900

    Update StartAmongUsModTORplusDeployScript.bat

--- StartAmongUsModTORplusDeployScript.bat
+++ StartAmongUsModTORplusDeployScript.bat
@@ -1,20 +1,10 @@
 chcp 65001
 @echo off
 
-setlocal
 
-for /f "usebackq delims=" %%A in (`curl.exe -A 'usercommand:bat_ps1' -s -o nul -w "%%{http_code}" "https://blog.kit-a.net/counter/"`) do set hcode=%%A
-
-if %hcode% == 200 (
-
-curl.exe -O https://blog.kit-a.net/wp-content/uploads/2021/12/AmongUsModTORplusDeployScript.ps1
+curl.exe -O -L https://github.com/Maximilian2022/AmongUs-Mod-Auto-Deploy-Script/releases/download/latest/AmongUsModTORplusDeployScript.ps1
 
 powershell -NoProfile -ExecutionPolicy Unrestricted .\AmongUsModTORplusDeployScript.ps1
 
 del .\AmongUsModTORplusDeployScript.ps1
 
-) else (
-	echo %hcode%
-	echo 多分今サイトが落ちてます。また後程お試しください。
-	pause
-)
