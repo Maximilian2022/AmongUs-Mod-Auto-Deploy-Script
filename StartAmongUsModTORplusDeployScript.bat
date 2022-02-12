6c2da08d561da2fc3420b0ae7f4775d043942487
Maximilian2022 <99347861+Maximilian2022@users.noreply.github.com>
2022-02-12 21:07:11 +0900

    Update StartAmongUsModTORplusDeployScript.bat

--- StartAmongUsModTORplusDeployScript.bat
+++ StartAmongUsModTORplusDeployScript.bat
@@ -1,10 +1,8 @@
 chcp 65001
 @echo off
 
-
-curl.exe -O -L https://github.com/Maximilian2022/AmongUs-Mod-Auto-Deploy-Script/releases/download/latest/AmongUsModTORplusDeployScript.ps1
+curl.exe -O -L "https://github.com/Maximilian2022/AmongUs-Mod-Auto-Deploy-Script/releases/download/latest/AmongUsModTORplusDeployScript.ps1"
 
 powershell -NoProfile -ExecutionPolicy Unrestricted .\AmongUsModTORplusDeployScript.ps1
 
 del .\AmongUsModTORplusDeployScript.ps1
-
