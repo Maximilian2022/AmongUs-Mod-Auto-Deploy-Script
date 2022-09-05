a0fc3cc288aef4de74da5cc4cac10673b13e7f8d
Maximilian2022 <99347861+Maximilian2022@users.noreply.github.com>
2022-09-05 22:21:23 +0900

    gomi

--- StartAmongUsGetLogScript_TOR GMH.bat
+++
@@ -1,7 +0,0 @@
-﻿chcp 65001
-    @echo off
-    curl.exe -O -L https://raw.githubusercontent.com/Maximilian2022/AmongUs-Mod-Auto-Deploy-Script/main/gmhtechsupport.ps1
-    pwsh -NoProfile -ExecutionPolicy Unrestricted .\gmhtechsupport.ps1 "TOR GMH" "C:\Program Files (x86)\Steam\steamapps\common\Among Us TOR GMH Mod" "steam"
-
-    del .\gmhtechsupport.ps1
-    
