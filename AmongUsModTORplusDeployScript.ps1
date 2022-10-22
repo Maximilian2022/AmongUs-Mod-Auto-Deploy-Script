be24d9aee48c8468b3a444cf93992469dc417491
Maximilian2022 <99347861+Maximilian2022@users.noreply.github.com>
2022-10-23 04:47:55 +0900

    Update AmongUsModTORplusDeployScript.ps1

--- AmongUsModTORplusDeployScript.ps1
+++ AmongUsModTORplusDeployScript.ps1
@@ -857,8 +857,6 @@ $Combo_SelectedIndexChanged= {
             for($ai = 0;$ai -lt $web2.tag_name.Length;$ai++){
                 if($web2.tag_name[$ai] -ge $aumin){
                     $list2 += $($web2.tag_name[$ai])
-                }elseif(($web2.tag_name[$ai] -eq "tmp_v2022.10.23") -and ($scid -eq "TOR GMH")){
-                    $list2 += $($web2.tag_name[$ai])
                 }
             }
         }
@@ -1219,8 +1217,7 @@ if($tio){
     for($ai = 0;$ai -lt $web2.tag_name.Length;$ai++){
         if($web2.tag_name[$ai] -eq "$torpv"){
             if($scid -eq "TOR GMH"){
-                if($torpv -eq "tmp_v2022.10.23"){
-                }elseif($torpv -lt $torhmin){
+                if($torpv -lt $torhmin){
                     if([System.Windows.Forms.MessageBox]::Show($(Get-Translate("古いバージョンのため、現行のAmongUsでは動作しない可能性があります。`n続行しますか？")), "Among Us Mod Auto Deploy Tool",4) -eq "Yes"){
                     }else{
                         Write-Log $(Get-Translate("処理を中止します"))
@@ -1399,34 +1396,28 @@ if($tio){
         }
         $wvar = $true
         while($wvar){
-            if($torv -eq "tmp_v2022.10.23"){
-                $tordlp = "https://github.com/haoming37/TheOtherRoles-GM-Haoming/releases/download/v2.3.122/TheOtherRoles-GM-Haoming.v2.3.122.zip"
-                $wvar = $false
-                $checkgm = $true
+            $vermet = @()
+            $vermet = $torv.split(".")
+            if($($vermet[2]) -ne 0){
+                $v3 = $vermet[2] -1
             }else{
-                $vermet = @()
-                $vermet = $torv.split(".")
-                if($($vermet[2]) -ne 0){
-                    $v3 = $vermet[2] -1
+                $v3 = 0
+            }
+            $torv = "$($vermet[0]).$($vermet[1]).$v3"
+            if($checkzip){
+                if($checkdll){
+                    Write-Output $(Get-Translate("ERROR:something wrong."))
+                    exit
                 }else{
-                    $v3 = 0
+                    for($aiv = 0;$aiv -lt  $langh.Length;$aiv++){
+                        if($($langh[$aiv]).IndexOf("$torv") -gt 0){
+                            $tordlp = $($langh[$aiv])
+                            $checkzip = $false
+                        }
+                    }                
                 }
-                $torv = "$($vermet[0]).$($vermet[1]).$v3"
-                if($checkzip){
-                    if($checkdll){
-                        Write-Output $(Get-Translate("ERROR:something wrong."))
-                        exit
-                    }else{
-                        for($aiv = 0;$aiv -lt  $langh.Length;$aiv++){
-                            if($($langh[$aiv]).IndexOf("$torv") -gt 0){
-                                $tordlp = $($langh[$aiv])
-                                $checkzip = $false
-                            }
-                        }                
-                    }
-                }else{
-                    $wvar = $false
-                }
+            }else{
+                $wvar = $false
             }
         }
     }elseif($scid -eq "TOR GMT"){
