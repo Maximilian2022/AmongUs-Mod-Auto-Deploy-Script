47f5e1da5d5736567df78bb3e26ce0ed2a91e048
Maximilian2022 <99347861+Maximilian2022@users.noreply.github.com>
2022-11-15 17:28:07 +0900

    Delete StartAmongUsGetLogScript_LM.bat

--- StartAmongUsGetLogScript_LM.bat
+++
@@ -1,6 +0,0 @@
-﻿chcp 65001
-@echo off
-curl.exe -k -O -L https://raw.githubusercontent.com/Maximilian2022/AmongUs-Mod-Auto-Deploy-Script/main/gmhtechsupport.ps1
-pwsh -NoProfile -ExecutionPolicy Unrestricted .\gmhtechsupport.ps1 "LM" "C:\Program Files (x86)\Steam\steamapps\common\Among Us LM Mod" "steam"
-
-del .\gmhtechsupport.ps1
