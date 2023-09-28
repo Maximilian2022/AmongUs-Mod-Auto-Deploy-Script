58883985ddc02c4bdc5d52fc1723b72746770744
Maximilian2022 <99347861+Maximilian2022@users.noreply.github.com>
2023-09-29 02:45:56 +0900

    Update StartAmongUsModTORplusDeployScript.bat

--- StartAmongUsModTORplusDeployScript.bat
+++ StartAmongUsModTORplusDeployScript.bat
@@ -2,11 +2,19 @@
 chcp 65001
 @echo off
 
+echo Loading...
+
 for /f "usebackq" %%t in (`CD`) do set COUNT=%%t
+
+echo Downloading Latest Powershell Script
 
 curl.exe -k -O -L https://raw.githubusercontent.com/Maximilian2022/AmongUs-Mod-Auto-Deploy-Script/main/AmongUsModTORplusDeployScript.ps1
 
+echo Running Powershell Script
+
 powershell -NoProfile -WindowStyle Minimized -ExecutionPolicy Unrestricted .\AmongUsModTORplusDeployScript.ps1
+
+echo Delete Powershell Script
 
 del /F /Q %COUNT%\AmongUsModTORplusDeployScript.ps1
 
