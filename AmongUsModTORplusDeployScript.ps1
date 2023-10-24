7438a2da9285215d50c26825d3657e708b4d4302
Maximilian2022 <99347861+Maximilian2022@users.noreply.github.com>
2023-10-24 18:26:19 +0900

    Update AmongUsModTORplusDeployScript.ps1

--- AmongUsModTORplusDeployScript.ps1
+++ AmongUsModTORplusDeployScript.ps1
@@ -2967,7 +2967,7 @@ if($tio){
             Copy-Item $aupatho -destination $aupathm -recurse
             if(Test-Path "$aupathm\Among Us\"){
                 robocopy "$aupathm\Among Us" "$aupathm" /unilog:C:\Temp\temp.log /E >nul 2>&1
-                Remove-Item "$aupathm\AmongUs" -recurse
+                Remove-Item "$aupathm\Among Us\" -recurse
                 $content = Get-content "C:\Temp\temp.log" -Raw -Encoding Unicode
                 Write-Log "`r`n $content"
                 Remove-Item "C:\Temp\temp.log" -Force                            
