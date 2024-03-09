a830f038d3265c47fa120efb6a64341d9186e010
Maximilian2022 <99347861+Maximilian2022@users.noreply.github.com>
2024-03-09 13:58:38 +0900

    Update AmongUsModTORplusDeployScript.ps1

--- AmongUsModTORplusDeployScript.ps1
+++ AmongUsModTORplusDeployScript.ps1
@@ -7,7 +7,7 @@ Write-Output "$Log PS1 Loading Start"
 # Among Us Mod Auto Deploy Script
 #
 $version = "2.0.0"
-$build = "20240308002"
+$build = "20240309001"
 #
 #################################################################################################
 ### minimum version for v2024.3.5
